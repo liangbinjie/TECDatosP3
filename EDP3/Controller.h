57a1807534a0ae42d7bece3bf6d03dc945e95653
liangbinjie <mailandresliang@gmail.com>
2023-10-30 00:31:04 -0600

    Include extern variables

--- EDP3/Controller.h
+++ EDP3/Controller.h
@@ -5,13 +5,36 @@
 #include "Ciudades.h"
 #include "Admins.h"
 
+// extern variables for Paises ----------------------------------------
 extern ArbolPais paises;
-extern ArbolCiudad ciudades;
-extern listaAdmins administradores;
-extern listaAdmins clientes;
-
 extern bool boolInsertarPais;
 extern bool boolBuscarPais;
 extern bool boolModificarPais;
+extern bool boolEliminarPais;
+
+
+// extern variables for ciudades ----------------------------------------
+extern ArbolCiudad ciudades;
+extern bool boolInsertarCiudad;
+extern bool boolBuscarCiudad;
+extern bool boolModificarCiudad;
+extern bool boolEliminarCiudad;
+
+
+// extern variables for clientes ----------------------------------------
+extern listaAdmins clientes;
+extern bool boolInsertarCliente;
+extern bool boolBuscarCliente;
+extern bool boolModificarCliente;
+extern bool boolEliminarCliente;
+
+
+// extern variables for admins ----------------------------------------
+extern listaAdmins administradores;
+extern bool boolInsertarAdmin;
+extern bool boolBuscarAdmin;
+extern bool boolModificarAdmin;
+extern bool boolEliminarAdmin;
+
 
 #endif
