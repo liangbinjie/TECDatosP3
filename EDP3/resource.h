5729f3338cfb9c7a62ea808f733cc5bc71713b82
liangbinjie <mailandresliang@gmail.com>
2023-10-30 19:43:45 -0600

    Mini modificacion

---
+++ EDP3/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusi¨®n generado de Microsoft Visual C++.
+// Usado por EDP3.rc
+//
+#define IDR_DESIGN1                     101
+#define IDR_DESIGN2                     102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
